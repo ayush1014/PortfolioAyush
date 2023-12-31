42ee94edb9fa42d4f36218617ff71fcae8b55c38
Ayush Kanaujia <ayushkanaujia14@gmail.com>
2023-12-31 04:28:31 -0600

    changed index and app file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+// import { HashRouter, Route, Routes } from 'react-router-dom';
+// import Home from './components/Home';
+// import About from './components/About';
+// import Contact from './components/Contact';
+// import Layout from './components/Layout';
+// import Portfolio from './components/Portfolio';
+// import './App.scss';
+
+// function App() {
+//   return (
+//     <HashRouter basename="/">
+//       <Routes>
+//         <Route path="/" element={<Layout />}>
+//           <Route index element={<Home />} />
+//           <Route path="about" element={<About />} />
+//           <Route path="portfolio" element={<Portfolio />} />
+//           <Route path="contact" element={<Contact />} />
+//         </Route>
+//       </Routes>
+//     </HashRouter>
+//   );
+// }
+
+// export default App;
+
 import Layout from './components/Layout';
-import Portfolio from './components/Portfolio';
 import './App.scss';
 
 function App() {
   return (
-    <HashRouter basename="/">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="portfolio" element={<Portfolio />} />
-          <Route path="contact" element={<Contact />} />
-        </Route>
-      </Routes>
-    </HashRouter>
+    <Layout/>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import reportWebVitals from './reportWebVitals'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import reportWebVitals from './reportWebVitals'
+// import { BrowserRouter } from 'react-router-dom'
+// import App from './App'
+// import './index.css'
 
-ReactDOM.render(
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//     ,
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
+
+// reportWebVitals()
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Layout from './components/Layout';
+import Portfolio from './components/Portfolio';
+import './styles/index.css';
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "about", element: <About /> },
+      { path: "portfolio", element: <Portfolio /> },
+      { path: "contact", element: <Contact /> },
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-    ,
-  </React.StrictMode>,
-  document.getElementById('root')
-)
-
-reportWebVitals()
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
